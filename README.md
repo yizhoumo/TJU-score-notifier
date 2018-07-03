70388ca8cf64bcf5cceb8327947f150f5d9e1ea3
rimo <yizhoumo@qq.com>
2018-07-03 23:42:27 +0800

    使用“统一身份认证”方式登录

--- README.md
+++ README.md
@@ -2,19 +2,19 @@
 Inspired by [Tongji-GPA](https://github.com/wlh320/Tongji-GPA).
 
 ## Usage
-Supposing you've modified the script and save it at `/home/rimo/tju-score.py`, simply run it with `python /home/rimo/tju-score.py`. If it failed, install the necessary components.
+Supposing you've modified the info in the script and save it at `/home/rimo/tju-score.py`, the simple usage is just like `python /home/rimo/tju-score.py`.
 
-**Attention**: Do NOT delete `tju-score.txt`, because it tracks the ID of updated courses.
+**Attention**: DO NOT delete `updated.txt`, because it tracks the ID of updated courses.
 
-Add a schedule by `crontab -e` and add a new line with:
+You can make a schedule by executing `crontab -e` and adding a new line like:
 ```
 0,30 8-22 * * * python /home/rimo/tju-score.py > /dev/null
 ```
-which means it will run at 8:00, 8:30, 9:00, ..., 22:30 on every single day. Feel free to modify the time.
+which means it will run at 8:00, 8:30, 9:00, ..., 22:30 on every single day. Feel free to modify the time, though.
 
-After saving the crontab, remember to restart the cron by `service cron restart` or something like that.
+After saving the crontab, don't forget to restart the cron by `service cron restart` or something similar.
 
-If you'd like to save the log file, try this in your crontab instead:
+If you'd like to save the log as well, try this instead:
 ```
 0,30 8-22 * * * date >> /home/rimo/tju-score.log
 0,30 8-22 * * * python /home/rimo/tju-score.py >> /home/rimo/tju-score.log
--- tju-score.py
+++ tju-score.py
@@ -1,85 +1,105 @@
-#coding:UTF-8
+# coding:UTF-8
 #!/usr/bin/python3
+
 import requests
-import webbrowser
 from lxml import etree
 import smtplib
 from email.mime.text import MIMEText
 from email.header import Header
 from email.utils import formataddr
-import urllib
-import sys
-reload(sys)
-sys.setdefaultencoding('utf8')
+import os
+import re
+import json
 
-### 编辑下列信息
-id = '1650000'          # 学号
-password = '12345678'   # 密码
-mail_host = 'smtp.qq.com'   # SMTP服务器
-mail_user = '10000@qq.com'  # 邮箱用户名
-mail_pass = '12345678'      # 邮箱密码
-sender = '10000@qq.com'     # 发件人
-receiver = '10000@qq.com'   # 收件人
-### 编辑上述信息
 
-login_url = 'http://tjis2.tongji.edu.cn:58080/amserver/UI/Login?service=adminconsoleservice&goto=http://tjis2.tongji.edu.cn:58080/amserver/base/AMAdminFrame&&IDToken1=' + \
-	id + '&IDToken2=' + urllib.quote(password)
-check_url = 'http://xuanke.tongji.edu.cn/pass.jsp'
-grade_url = 'http://xuanke.tongji.edu.cn/tj_login/redirect.jsp?link=/tj_xuankexjgl/score/query/student/cjcx.jsp?qxid=20051013779916$mkid=20051013779901&qxid=20051013779916'
+# 编辑下列信息
+USER_ID   = '1650000'		# 学号
+PASSWORD  = 'password'      # 密码
+SEMESTER  = '20172'			# 学期
+MAIL_HOST = 'smtp.qq.com'   # SMTP服务器
+MAIL_USER = '10000@qq.com'  # 邮箱用户名
+MAIL_PASS = 'mail_pwd'      # 邮箱密码
+SENDER    = '10000@qq.com'  # 发件人
+RECEIVER  = '10086@qq.com'  # 收件人
+# 编辑上述信息
 
-# go and get grade!
-session = requests.Session()
+UA = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 ' +\
+    '(KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36'
+UPDATED = 'updated.json'
+
 try:
-	req1 = session.get(login_url, timeout=10)
-	req2 = session.get(check_url, timeout=10)
+    session = requests.Session()
+    session.cookies.update({'User-Agent': UA})
+
+    url_login = 'http://xuanke.tongji.edu.cn:443/oiosaml/saml/login'
+    r = session.get(url_login)
+
+    url_request = re.findall(r'url=(\S*)"></head>', r.text)[0]
+    session.get(url_request)
+
+    url_sso = 'https://ids.tongji.edu.cn:8443/nidp/saml2/sso?sid=0'
+    session.post(url_sso)
+    session.post(url_sso, data={
+        'option': 'credential',
+        'Ecom_User_ID': USER_ID,
+        'Ecom_Password': PASSWORD,
+        'submit': '登录'
+    })
+    r = session.get(url_sso)
+
+    url_assert = 'http://xuanke.tongji.edu.cn:443/oiosaml/saml/SAMLAssertionConsumer'
+    SAMLResponse = re.findall(
+        r'name="SAMLResponse" value="(\S*)"/>', r.text)[0]
+    session.post(url_assert, data={'SAMLResponse': SAMLResponse})
+
+    url_redirect = 'http://xuanke.tongji.edu.cn/tj_login/redirect.jsp?' +\
+        'link=/tj_xuankexjgl/score/query/student/cjcx.jsp?qxid=20051013779916$mkid=20051013779901'
+    session.get(url_redirect)
+
+    url_cjcx = 'http://xuanke.tongji.edu.cn/tj_xuankexjgl/score/query/' +\
+        'student/cjcx.jsp?qxid=20051013779916&mkid=20051013779901'
+    r = session.post(url_cjcx, data={
+        'qx': '0',
+        'xndxq': SEMESTER,
+        'xklx': ''
+    })
 except:
-	print('Error: 网络连接失败')
-	exit()
+    print('Error: 登录失败')
+    exit()
 
-if not('失败' in req2.text):
-	try:
-		req3 = session.get(grade_url, timeout=10)
-	except:
-		print('Error: 网络连接失败')
-		exit()
+html = etree.HTML(r.text)
+table = html.xpath('//table[@class="mainTable"]/tr[td/div/font]')
 
-	text = req3.text
-	html = etree.HTML(text)
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-	table = html.xpath('//table[@class="mainTable"]/tr[td/div/font]')
+updateList = []
+with open(UPDATED, 'r') as fin:
+    courseList = json.load(fin)
 
-	file = open(sys.path[0] + '/tju-score.txt', 'a+')
-	courseList = file.read().split()
+for tr in table:
+    line = tr.xpath('td/div/font/text()')
+    if line[0] in courseList:
+        continue
+    courseList.append(line[0])
+    updateList.append('<td>' + '</td><td>'.join(line[1:4]) + '</td>')
 
-	updateList = []
+with open(UPDATED, 'w') as fout:
+    json.dump(courseList, fout)
 
-	for tr in table:
-		line = tr.xpath('td/div/font/text()')
-		if line[0] in courseList:
-			continue
-		courseList.append(line[0])
-		file.write(line[0] + '\n')
-		updateList.append('<td>' + '</td><td>'.join(line[1:4]) + '</td>')
-
-	file.close()
-
-	if len(updateList):
-		msgText = '<table border="1" rules="all"><tr>' + '</tr><tr>'.join(updateList) + '</tr></table>'
-
-		message = MIMEText(msgText, 'html', 'utf-8')
-		message['From'] = formataddr([sender, sender])
-		message['To'] = formataddr([receiver, receiver])
-		message['Subject'] = Header('成绩更新'+str(len(courseList)), 'utf-8')
-
-		try:
-			smtpObj = smtplib.SMTP(mail_host, 25)
-			#smtpObj.set_debuglevel(1)
-			smtpObj.login(mail_user, mail_pass)
-			smtpObj.sendmail(sender, [receiver], message.as_string())
-			print '邮件发送成功'
-		except smtplib.SMTPException:
-			print 'Error: 无法发送邮件'
-	else:
-		print('没有成绩更新')
+if len(updateList):
+    msgText = '<table border="1" rules="all"><tr>' + \
+        '</tr><tr>'.join(updateList) + '</tr></table>'
+    message = MIMEText(msgText, 'html', 'utf-8')
+    message['From'] = formataddr([SENDER, SENDER])
+    message['To'] = formataddr([RECEIVER, RECEIVER])
+    message['Subject'] = Header('成绩更新{%d}' % len(courseList), 'utf-8')
+    try:
+        smtpObj = smtplib.SMTP(MAIL_HOST, 25)
+        # smtpObj.set_debuglevel(1)
+        smtpObj.login(MAIL_USER, MAIL_PASS)
+        smtpObj.sendmail(SENDER, [RECEIVER], message.as_string())
+        print('邮件发送成功')
+    except smtplib.SMTPException:
+        print('Error: 无法发送邮件')
 else:
-	print('Error: 登陆失败!')
+    print('没有成绩更新')
